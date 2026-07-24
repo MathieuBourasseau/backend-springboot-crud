fa5e990cd727eeed30d7269d015dd9ab49df9799
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-07-24 19:53:24 +0200

    feat: ProductController.java created. RequestMapping set up. getAllProducts and getProductById methods added in controller

---
+++ src/main/java/com/mathieu/crud/controllers/ProductController.java
@@ -0,0 +1,36 @@
+package com.mathieu.crud.controllers;
+
+import com.mathieu.crud.dto.ProductCreateDto;
+import com.mathieu.crud.models.Product;
+import com.mathieu.crud.services.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        try {
+            Product product = productService.getProductById(id);
+            return ResponseEntity.ok(product);
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
